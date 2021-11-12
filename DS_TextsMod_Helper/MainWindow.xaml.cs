027cf39c11440fa94c094eeafd4694bb5c7c5543
François MAZOYER <francois.mazoyer@outlook.fr>
2021-11-12 19:03:42 +0100

    Ajoutez des fichiers projet.

---
+++ DS_TextsMod_Helper/MainWindow.xaml.cs
@@ -0,0 +1,457 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DS_TextsMod_Helper
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //TODO: [General] Reorganize code in several files/classes
+
+
+
+
+        // Hacky shortcuts // TODO: Improve
+        private static bool compare_clicked = false;
+        private static bool prepare_clicked = false;
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Directory.CreateDirectory(ReturnOutputDirectoryPath());
+        }
+
+
+
+
+        private string ReturnOutputDirectoryPath()
+        {
+            string src_executable = Assembly.GetExecutingAssembly().Location;
+            string root_directory = Directory.GetParent(src_executable).ToString();
+            return Path.Combine(root_directory, "Output");
+        }
+
+
+
+
+        // ----------
+        // GUI events
+        // ----------
+        private void tbx_file1_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void tbx_file1_Drop(object sender, DragEventArgs e)
+        {
+            if (AcceptDroppedInputFile(e))
+                DisplayInputFilepath(sender, e);
+        }
+
+        private void tbx_file2_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void tbx_file2_Drop(object sender, DragEventArgs e)
+        {
+            if (AcceptDroppedInputFile(e))
+                DisplayInputFilepath(sender, e);
+        }
+
+        private void tbx_file3_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void tbx_file3_Drop(object sender, DragEventArgs e)
+        {
+            if (AcceptDroppedInputFile(e))
+                DisplayInputFilepath(sender, e);
+        }
+
+
+
+
+        private void btn_comparefiles_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: "Disable" useless controls for this mode : tbx_file3
+
+            btn_execute.IsEnabled = false;
+
+            compare_clicked = true;
+            prepare_clicked = false;
+
+            tbk_summary.Text = "[Compare/Compile] To generate a new file with data comparison\n"
+                             + "Expected: File #1 is the modded text file in {{Mod author's language}}. File #2 is the same file in Vanilla version, in same language.\n"
+                             + "The program will scan contents of both files, from their Text IDs and values:\n"
+                             + "    For a given Text ID: do the files have a value and is it the same for both?\n"
+                             + "        None of them has a value: skip the TextID/the line\n"
+                             + "        At least one of them has a value: compare the values and give the information \"Are both values the same?\"\n";
+
+            if (CompareModeReady())
+            {
+                SortedDictionary<int, string> sdict = ReturnCompareDictionary(tbx_file1.Text, tbx_file2.Text, true);
+                PreviewCompare(sdict, tbx_header1.Text, tbx_header2.Text);
+
+                btn_execute.IsEnabled = true;
+            }
+        }
+
+        private void btn_preparefile_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: "Disable" useless controls for this mode : tbx_header1, tbx_header2
+
+            btn_execute.IsEnabled = false;
+
+            compare_clicked = false;
+            prepare_clicked = true;
+
+            tbk_summary.Text = "[Prepare/Comply] To prepare a new file for mod translation\n"
+                             + "Expected: File #1 is the modded text file in {{Mod author's language}}. File #2 is the same file in Vanilla version, in same language. "
+                             + "File #3 is the same file in {{target new language}}.\n"
+                             + "The program will replicate structure of file #1 regarding to Text IDs, and compare data from #1 and #2 regarding to values:\n"
+                             + "    For a given Text ID, are their values the same?\n"
+                             + "        Yes, files #1 and #2 has the same value: use value from file #3, as this is the vanilla content and translation already exists\n"
+                             + "        No, files #1 and #2 has different values: for the translator to complete manually";
+
+            if (PrepareModeReady())
+            {
+                Dictionary<int, string> dict = ReturnPrepareDictionary(tbx_file1.Text, tbx_file2.Text, tbx_file3.Text, true);
+                PreviewPrepare(dict);
+
+                btn_execute.IsEnabled = true;
+            }
+        }
+
+
+
+
+        private void btn_execute_Click(object sender, RoutedEventArgs e)
+        {
+            if (compare_clicked)
+            {
+                SortedDictionary<int, string> sdict = ReturnCompareDictionary(tbx_file1.Text, tbx_file2.Text, false);
+                DoCompare(sdict, tbx_header1.Text, tbx_header2.Text, tbx_outputfilename.Text);
+
+                MessageBox.Show(string.Format("Compare mode: File \"{0}\" created.", tbx_outputfilename.Text));
+            }
+
+            if (prepare_clicked)
+            {
+                Dictionary<int, string> dict = ReturnPrepareDictionary(tbx_file1.Text, tbx_file2.Text, tbx_file3.Text, false);
+                DoPrepare(dict, tbx_outputfilename.Text);
+
+                MessageBox.Show(string.Format("Prepare mode: File \"{0}\" created.", tbx_outputfilename.Text));
+            }
+        }
+
+
+        // --------------------
+        // Validation & display
+        // --------------------
+        private bool AcceptDroppedInputFile(DragEventArgs e)
+        {
+            FileInfo file_info = new FileInfo(((string[])e.Data.GetData(DataFormats.FileDrop))[0]);
+            if (file_info.Extension.ToLowerInvariant() != ".csv")
+                return false;
+
+            string[] lines = File.ReadAllLines(file_info.FullName);
+            if (lines.Length < 3)
+                return false;
+
+            return true;
+        }
+
+        private void DisplayInputFilepath(object sender, DragEventArgs e)
+        {
+            string filepath = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            TextBox tbx = sender as TextBox;
+
+            tbx.Text = filepath;
+            tbx.FontStyle = FontStyles.Normal;
+            tbx.Foreground = Brushes.Black;
+        }
+
+        private bool CompareModeReady()
+        {
+            string input_filepath1 = tbx_file1.Text;
+            string input_filepath2 = tbx_file2.Text;
+            string output_header_1 = tbx_header1.Text;
+            string output_header_2 = tbx_header2.Text;
+            string output_filename = tbx_outputfilename.Text + ".csv";
+
+            if (!File.Exists(input_filepath1) || !File.Exists(input_filepath2))
+            {
+                MessageBox.Show("Error : missing input file(s)");
+                return false;
+            }
+
+            if (input_filepath1 == input_filepath2)
+            {
+                MessageBox.Show("Error : same file submitted several times");
+                return false;
+            }
+
+            if (output_filename == ".csv")
+            {
+                MessageBox.Show("Error : output filename not specified");
+                return false;
+            }
+
+            if (output_header_1 == "" || output_header_2 == "")
+            {
+                MessageBox.Show("Error : empty output headers");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool PrepareModeReady()
+        {
+            string input_filepath1 = tbx_file1.Text;
+            string input_filepath2 = tbx_file2.Text;
+            string input_filepath3 = tbx_file3.Text;
+            string output_filename = tbx_outputfilename.Text + ".csv";
+
+            if (!File.Exists(input_filepath1) || !File.Exists(input_filepath2) || !File.Exists(input_filepath3))
+            {
+                MessageBox.Show("Error : missing input file(s)");
+                return false;
+            }
+
+            if ((input_filepath1 == input_filepath2) || (input_filepath2 == input_filepath3) || (input_filepath1 == input_filepath3))
+            {
+                MessageBox.Show("Error : same file submitted several times");
+                return false;
+            }
+
+            if (output_filename == ".csv")
+            {
+                MessageBox.Show("Error : output filename not specified");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+
+        // ----------------------
+        // Calling core functions
+        // ----------------------
+        private void PreviewCompare(SortedDictionary<int, string> dictionary_compare, string oheader1, string oheader2)
+        {
+            // Display output_data in Preview
+            string output_preview = string.Format("Text ID|{0}|{1}|Same?", oheader1, oheader2);
+            foreach (KeyValuePair<int, string> od in dictionary_compare)
+            {
+                output_preview += string.Format("\n{0}", od.Value);
+            }
+
+            tbk_preview.Text = output_preview;
+        }
+
+        private void PreviewPrepare(Dictionary<int, string> dictionary_prepare)
+        {
+            // Display output_data in Preview
+            string output_preview = string.Format("Text ID|Value");
+            foreach (KeyValuePair<int, string> od in dictionary_prepare)
+            {
+                output_preview += string.Format("\n{0}|{1}", od.Key, od.Value);
+            }
+
+            tbk_preview.Text = output_preview;
+        }
+
+
+
+
+        private void DoCompare(SortedDictionary<int, string> sdict, string oheader1, string oheader2, string output_filepath)
+        {
+            using (StreamWriter writer = new StreamWriter(output_filepath, false))
+            {
+                writer.WriteLine($"Text ID|{oheader1}|{oheader2}|Same?");
+                foreach (KeyValuePair<int, string> od in sdict)
+                {
+                    writer.WriteLine(od.Value);
+                }
+            }
+        }
+
+        private void DoPrepare(Dictionary<int, string> dict, string output_filepath)
+        {
+            using (StreamWriter writer = new StreamWriter(output_filepath, false))
+            {
+                writer.WriteLine("Text ID|Value");
+                foreach (KeyValuePair<int, string> od in dict)
+                {
+                    writer.WriteLine($"{od.Key}|{od.Value}");
+                }
+            }
+        }
+
+
+
+
+        // --------------
+        // Core functions
+        // --------------
+        private SortedDictionary<int,string> ReturnCompareDictionary(string ifile1, string ifile2, bool preview)
+        {
+            // Get input data from both files
+            string[] file_1_lines = File.ReadAllLines(ifile1);
+            string[] file_2_lines = File.ReadAllLines(ifile2);
+
+            int line_counter = 0;
+
+            SortedDictionary<int, string> output_dictionary = new SortedDictionary<int, string>();
+
+            // First, fill the dictionary with values from File1
+            foreach (string line in file_1_lines)
+            {
+                string str_id = line.Split('|')[0].Trim();
+                string value1 = line.Split('|')[1].Trim();
+
+                if (value1 == "")
+                    continue; // Exclude lines without value
+
+                if (int.TryParse(str_id, out int id))
+                {
+                    output_dictionary.Add(id, value1);
+                    line_counter += 1;
+                }
+
+                if (preview && line_counter == 6)
+                    break;
+            }
+
+
+            // Then, compare File1 values against File2 ones
+            line_counter = 0;
+            foreach (string line in file_2_lines)
+            {
+                string str_id = line.Split('|')[0].Trim();
+                string value2 = line.Split('|')[1].Trim();
+
+                if (value2 == "")
+                    continue; // Exclude lines without value
+
+                if (int.TryParse(str_id, out int id))
+                {
+                    if (output_dictionary.ContainsKey(id)) // [File2 ID] in [File1 IDs] : we have the values from both File1 and File2
+                    {
+                        if (output_dictionary.TryGetValue(id, out string value1))
+                            output_dictionary[id] = string.Format("{0}|{1}|{2}|{3}", id, value1, value2, (value1 == value2) ? "true" : "false");
+                    }
+                    else // [File2 ID] not in [File1 IDs] : we only have the value from File2
+                        output_dictionary.Add(id, string.Format("{0}||{1}|false", id, value2));
+
+                    line_counter += 1;
+                }
+
+                if (preview && line_counter == 6)
+                    break;
+            }
+
+
+            // Finally, get back on formatting dictionary entries for File1
+            SortedDictionary<int, string> replica_dictionary = new SortedDictionary<int, string>(output_dictionary);
+            foreach (KeyValuePair<int, string> rd in replica_dictionary)
+            {
+                if (!rd.Value.Contains("|true") && !rd.Value.Contains("|false")) // [File1 ID] not in [File2 IDs] : we only have the value from File1
+                {
+                    if (replica_dictionary.TryGetValue(rd.Key, out string value1))
+                    {
+                        output_dictionary[rd.Key] = string.Format("{0}|{1}||false", rd.Key, value1);
+                    }
+                }
+            }
+
+            return output_dictionary;
+        }
+
+
+
+
+        private Dictionary<int, string> ReturnPrepareDictionary(string ifile1, string ifile2, string ifile3, bool preview)
+        {
+            // Get input data from both files
+            string[] file_1_lines = File.ReadAllLines(ifile1);
+            string[] file_2_lines = File.ReadAllLines(ifile2);
+            string[] file_3_lines = File.ReadAllLines(ifile3);
+
+            int line_counter = 0;
+
+            Dictionary<int, string> output_dictionary = new Dictionary<int, string>();
+
+
+            foreach (string line in file_1_lines)
+            {
+                string str_id = line.Split('|')[0].Trim();
+                string value1 = line.Split('|')[1].Trim();
+
+                if (int.TryParse(str_id, out int id))
+                {
+                    line_counter += 1;
+                    output_dictionary.Add(id, value1); // Contains File1 value so far
+                }
+
+                if (preview && line_counter >= 6)
+                    break;
+            }
+
+
+            foreach (string line in file_2_lines)
+            {
+                string str_id = line.Split('|')[0].Trim();
+                string value2 = line.Split('|')[1].Trim();
+
+                if (int.TryParse(str_id, out int id))
+                {
+                    if (output_dictionary.ContainsKey(id))
+                    {
+                        if (output_dictionary.TryGetValue(id, out string value1))
+                        {
+                            if (value1 == value2)
+                                output_dictionary[id] = ""; // Erase, to be replaced with File3 value
+                        }
+                    }
+                    // else : File2 value ignored as not also in File #1
+                }
+            }
+
+
+            foreach (string line in file_3_lines)
+            {
+                string str_id = line.Split('|')[0].Trim();
+                string value3 = line.Split('|')[1].Trim();
+
+                if (int.TryParse(str_id, out int id))
+                {
+                    if (output_dictionary.ContainsKey(id))
+                    {
+                        if (output_dictionary.TryGetValue(id, out string value1))
+                        {
+                            if (value1 == "")
+                                output_dictionary[id] = value3; // Insert File #3 value
+                        }
+                    }
+                    // else : File3 value ignored as not also in File #1
+                }
+            }
+
+            return output_dictionary;
+        }
+
+    }
+}
