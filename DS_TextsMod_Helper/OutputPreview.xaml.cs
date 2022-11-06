f871670f744b246c39c1965de5502c0ae56170d2
François MAZOYER <francois.mazoyer@outlook.fr>
2022-11-06 13:34:45 +0100

    Add contents highlighting in Preview

--- DS_TextsMod_Helper/OutputPreview.xaml.cs
+++ DS_TextsMod_Helper/OutputPreview.xaml.cs
@@ -12,18 +12,21 @@ namespace DS_TextsMod_Helper
             if (!(processingMode.AllReadModeEntries is null))
             {
                 Title = "Output preview : Read mode";
+                Cbx_HighlightItems.Visibility = Visibility.Collapsed;
                 Tbc_Preview_Read.Visibility = Visibility.Visible;
                 Tbc_Preview_Read.ItemsSource = processingMode.AllReadModeEntries;
             }
             if (!(processingMode.AllCompareModeEntries is null))
             {
                 Title = "Output preview : Compare mode";
+                Cbx_HighlightItems.Content = "Highlight contents (same / different)";
                 Tbc_Preview_Compare.Visibility = Visibility.Visible;
                 Tbc_Preview_Compare.ItemsSource = processingMode.AllCompareModeEntries;
             }
             if (!(processingMode.AllPrepareModeEntries is null))
             {
                 Title = "Output preview : Prepare mode";
+                Cbx_HighlightItems.Content = "Highlight contents (translated / not translated)";
                 Tbc_Preview_Prepare.Visibility = Visibility.Visible;
                 Tbc_Preview_Prepare.ItemsSource = processingMode.AllPrepareModeEntries;
             }
